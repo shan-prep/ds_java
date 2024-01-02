1f9f1178cbaebb6ed2db876958111b2d19482dd5
Shanmugavel Sundaramoorthy <Shanmugavel@users.noreply.github.com>
2024-01-02 14:15:51 +0530

    DFS -- all 3 orders

--- src/main/java/xyz/shanmugavel/prep/interview/ds/dfs/DFS.java
+++ src/main/java/xyz/shanmugavel/prep/interview/ds/dfs/DFS.java
@@ -2,6 +2,7 @@ package xyz.shanmugavel.prep.interview.ds.dfs;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringJoiner;
 import xyz.shanmugavel.prep.interview.ds.GetInput;
 
@@ -10,7 +11,64 @@ public class DFS {
   public static void main(String[] args) {
     List<Integer> nos =  GetInput.readBinaryTree();
     Node rootNode = convertInputToBinaryTree(nos);
-    System.out.println(rootNode);
+    //traverseTreeUsingPreOrderWithoutRecursion(rootNode);
+    System.out.println("*************PRE ORDER Begins************");
+    traverseTreeUsingPreOderRecursively(rootNode);
+    System.out.println();
+    System.out.println("*************IN ORDER Begins************");
+    traverseTreeUsingInOderRecursively(rootNode);
+    System.out.println();
+    System.out.println("*************POST ORDER Begins************");
+    traverseTreeUsingPostOderRecursively(rootNode);
+  }
+
+  private static void traverseTreeUsingPreOderRecursively(Node rootNode) {
+    // visit root node, traverse left , traver right
+    if ( null == rootNode ) {
+      return;
+    }
+
+    System.out.print(" " + rootNode.getValue());
+    traverseTreeUsingPreOderRecursively(rootNode.getLeftNode());
+    traverseTreeUsingPreOderRecursively(rootNode.getRightNode());
+  }
+  private static void traverseTreeUsingInOderRecursively(Node rootNode) {
+  // traverse left , visit root node repeat , traver right
+    if ( null == rootNode ) {
+      return;
+    }
+    traverseTreeUsingInOderRecursively(rootNode.getLeftNode());
+    System.out.print(" " + rootNode.getValue());
+    traverseTreeUsingInOderRecursively(rootNode.getRightNode());
+  }
+  private static void traverseTreeUsingPostOderRecursively(Node rootNode) {
+// traverse right , traver left, visit root node repeat
+    if ( null == rootNode ) {
+      return;
+    }
+    traverseTreeUsingPostOderRecursively(rootNode.getLeftNode());
+    traverseTreeUsingPostOderRecursively(rootNode.getRightNode());
+    System.out.print(" " + rootNode.getValue());
+  }
+
+
+  private static void traverseTreeUsingPreOrderWithoutRecursion(Node rootNode) {
+    System.out.println("PreOrder -- BEGIN.");
+    Stack<Node> stack = new Stack<>();
+    stack.push(rootNode);
+    Node currentNode;
+    while( !stack.isEmpty() ) {
+      currentNode = stack.pop();
+      System.out.println(currentNode.getValue());
+      currentNode.setVisited(true);
+      if ( null != currentNode.getRightNode() ) {
+        stack.push(currentNode.getRightNode());
+      }
+      if ( null != currentNode.getLeftNode() ) {
+        stack.push(currentNode.getLeftNode());
+      }
+    }
+    System.out.println("PreOrder -- END.");
   }
 
   private static Node convertInputToBinaryTree(List<Integer> nos) {
